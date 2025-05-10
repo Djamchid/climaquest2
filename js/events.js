3cd1ce787a24bbfd4b012d0d79af47ae89cc8b7f
Djamchid <djamchid@gmail.com>
2025-05-10 12:39:29 +0200

    Add files via upload

---
+++ js/events.js
@@ -0,0 +1,17 @@
+export default [
+  {yearOffset:1, description:'Vague de chaleur extrême', effects:{temp:0.02}},
+  {yearOffset:2, description:'Inondations côtières majeures', effects:{sea:0.02}},
+  {yearOffset:3, description:'Découverte d'algues absorbant CO2', effects:{co2:-0.5}},
+  {yearOffset:4, description:'Sécheresse prolongée', effects:{biodiversity:-0.2}},
+  {yearOffset:5, description:'Sommet climatique mondial réussi', effects:{co2:-1}},
+  {yearOffset:6, description:'Ouragan de catégorie 5', effects:{temp:0.01, sea:0.01}},
+  {yearOffset:7, description:'Progrès dans les batteries', effects:{co2:-0.7}},
+  {yearOffset:8, description:'Incendies forestiers massifs', effects:{co2:0.8, biodiversity:-0.3}},
+  {yearOffset:9, description:'Accord sur le méthane', effects:{temp:-0.005}},
+  {yearOffset:10, description:'Nouvelle technologie solaire', effects:{co2:-1.2}},
+  {yearOffset:11, description:'Fonte record des glaces', effects:{sea:0.03}},
+  {yearOffset:12, description:'Biodiversité protégée', effects:{biodiversity:0.4}},
+  {yearOffset:13, description:'Découverte de plastique biodégradable', effects:{biodiversity:0.1}},
+  {yearOffset:14, description:'Tempête de sable géante', effects:{temp:0.005}},
+  {yearOffset:15, description:'Réduction des subventions fossiles', effects:{co2:-0.9}}
+];

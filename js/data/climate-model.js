c3ff28c7575317064bf5d678f67edfa92dea5521
Djamchid <djamchid@gmail.com>
2025-05-10 12:43:58 +0200

    Add files via upload

---
+++ js/data/climate-model.js
@@ -0,0 +1,8 @@
+export const DEFAULT_STATE = {
+  year:2025,
+  budget:10,
+  co2:410,        // ppm
+  temp:1.1,       // +°C vs préindustriel
+  sea:0,          // mètres vs 2025
+  biodiversity:0  // index simplifié (-5 à +5)
+};
